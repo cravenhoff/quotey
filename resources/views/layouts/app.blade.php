dc8d25e816a30f530289ad81bca5c5ffd42e72f3
Crystal Kewe <49733508+cravenhoff@users.noreply.github.com>
2023-06-02 16:36:56 +1000

    Added tailwindcss to laravel project
    Tailwindcss is successfully installed in the laravel project.

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,6 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Quotey - Share a quote, inspire and rank</title>
+    @vite('resources/css/app.css')
 </head>
 <body>
     <!-- Use blade directive to add placeholder for content from other pages to be display using this app layouts file -->
