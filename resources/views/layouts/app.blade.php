bdf4422a95ac949eed0faf30c00befcbf6d37e6a
Crystal Kewe <49733508+cravenhoff@users.noreply.github.com>
2023-06-02 16:19:53 +1000

    Added new bare layouts template
    Removed the default welcome.blade.php view file and replaced it with new app layout file.

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Quotey - Share a quote, inspire and rank</title>
+</head>
+<body>
+    <!-- Use blade directive to add placeholder for content from other pages to be display using this app layouts file -->
+    @yield('content')
+</body>
+</html>
