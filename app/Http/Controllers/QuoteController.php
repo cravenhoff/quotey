d168f1f276884caad14bf56d95a02c4852ccd0b2
Crystal Kewe <49733508+cravenhoff@users.noreply.github.com>
2023-06-08 21:27:42 +1000

    Created 'add quotes' form
    In addition to the form, also created the index() and store() methods in the controller, and connected the get and post routes for '/quotes' to the controller.

--- app/Http/Controllers/QuoteController.php
+++ app/Http/Controllers/QuoteController.php
@@ -6,5 +6,21 @@ use Illuminate\Http\Request;
 
 class QuoteController extends Controller
 {
-    //
+    // Create index() method that serves 'quotes' view
+    public function index() {
+        return view('quotes.index');
+    }
+
+    // Create store() method that processes the add quote 'form' submission
+    public function store(Request $request) {
+        // dd('post quote');
+
+        /* ----- VALIDATION ----- */
+        // Validate form using validate() method of the base controller.
+        $this->validate($request, [
+            'body' => 'required|max:555',
+        ]); // if fails, throws an exception
+
+        
+    }
 }
