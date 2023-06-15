0c148cf942e0018afc002b7a617ca094f4891ac1
Crystal Kewe <49733508+cravenhoff@users.noreply.github.com>
2023-06-16 01:04:58 +1000

    Retrieved, saved and piped through the list of quotes belonging to each user.

--- app/Http/Controllers/UserQuoteController.php
+++ app/Http/Controllers/UserQuoteController.php
@@ -9,8 +9,12 @@ class UserQuoteController extends Controller
 {
     // Create index() method that serves the detailed page view of a particular user and all their registered quotes
     public function index(User $user) {
+        // Use eager loading and pagination to retrieve the list of user's quotes
+        $quotes = $user->quotes()->with(['user', 'likes'])->paginate(3);
+
         return view('users.quotes.index', [
             'user' => $user,
+            'quotes' => $quotes,
         ]);
     }
 }
