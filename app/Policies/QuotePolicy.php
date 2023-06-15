ad62ced5b05be7b16b16e4f0d2a98a6ff9816f1f
Crystal Kewe <49733508+cravenhoff@users.noreply.github.com>
2023-06-15 22:45:00 +1000

    Create QuotePolicy.php

---
+++ app/Policies/QuotePolicy.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Policies;
+
+use App\Models\User;
+
+class QuotePolicy
+{
+    /**
+     * Create a new policy instance.
+     */
+    public function __construct()
+    {
+        //
+    }
+}
