c71b916b8a3a3dee66bb9487fe583a3df88fa78c
Crystal Kewe <49733508+cravenhoff@users.noreply.github.com>
2023-06-15 23:11:41 +1000

    Created the index view for the UserQuoteController
    Outputted the user's name, for now, when their profile name is clicked on.

--- app/Http/Controllers/UserQuoteController.php
+++ app/Http/Controllers/UserQuoteController.php
@@ -9,6 +9,8 @@ class UserQuoteController extends Controller
 {
     // Create index() method that serves the detailed page view of a particular user and all their registered quotes
     public function index(User $user) {
-        dd($user);
+        return view('users.quotes.index', [
+            'user' => $user,
+        ]);
     }
 }
