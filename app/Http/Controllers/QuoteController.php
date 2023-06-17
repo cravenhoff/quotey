06ce4acfdba3669ed3372d45ee52bf5b495dc7b2
Crystal Kewe <49733508+cravenhoff@users.noreply.github.com>
2023-06-17 20:43:20 +1000

    Added middleware to protect access to the 'store' and 'destroy' methods.
    This ensures that guests (unauthenticated users) cannot store and delete quotes - to do so, they must first login, or sign up if they don't have an account.

--- app/Http/Controllers/QuoteController.php
+++ app/Http/Controllers/QuoteController.php
@@ -7,6 +7,10 @@ use Illuminate\Http\Request;
 
 class QuoteController extends Controller
 {
+    public function __construct() {
+        $this->middleware(['auth'])->only(['store', 'destroy']);
+    }
+
     // Create index() method that serves 'quotes' view
     public function index() {
         /* ----- RETRIEVE LIST OF QUOTE ENTRIES IN DATABASE ----- */
